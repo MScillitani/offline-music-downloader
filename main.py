657cf8bbfe96170b3db9eec10a7b58bc44155f06
Matthew Scillitani <mattscil@gmail.com>
2023-03-18 03:48:04 -0400

    improved formatting and logic

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ from tkinter import ttk
 from tkinter.messagebox import showerror
 
 
-root = tk.Tk()
-root.title("Offline Music Downloader")
-root.geometry('400x250+50+50')
-root.resizable(False, False)
-
-link = tk.StringVar()
-
-
-def button_clicked():
+def button_click():
     if ("youtube" or ".com/") not in link.get().lower():
         showerror('Invalid URL', 'You must use a valid YouTube URL')
         return
@@ -20,12 +12,19 @@ def button_clicked():
     youtube_music = youtube_music.streams.get_audio_only()  # specifies audio
     try:
         youtube_music.download()  # attempts to download YouTube audio
-        success_label = ttk.Label(root, text="Download Successful")
-        success_label.pack(expand=False, pady=10)
-    except:
-        error_label = ttk.Label(root, text="Something went wrong. Please try again.")
-        error_label.pack(expand=False, pady=10)
+        download_label = ttk.Label(root, text="Download Successful")
+        download_label.pack(expand=False, pady=10)
+    except IOError:
+        download_label = ttk.Label(root, text="Something went wrong. Please try again.")
+        download_label.pack(expand=False, pady=10)
 
+
+root = tk.Tk()
+root.title("Offline Music Downloader")
+root.geometry('400x250+50+50')
+root.resizable(False, False)
+
+link = tk.StringVar()
 
 link_label = ttk.Label(root, text="Enter YouTube URL:")
 link_label.pack(expand=False, padx=50, pady=40)
@@ -34,7 +33,7 @@ link_entry = ttk.Entry(root, textvariable=link)
 link_entry.pack(fill='x', expand=False, padx=20)
 link_entry.focus()
 
-button = ttk.Button(root, text='Download', command=button_clicked)
+button = ttk.Button(root, text='Download', command=button_click)
 
 button.pack(side="bottom", fill='both', expand=False, padx=150, pady=30)
 
